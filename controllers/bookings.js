2ef2694ee4023fbde778e7d3745481eae54eeaa0
ahq_292 <ali.quraishy2014@email.com>
2025-11-07 00:30:36 +0300

    post booking and handle overlap

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -17,3 +17,28 @@ catch (err) {
     res.status(500).json({message: err.message})
 }
 })
+router.post("/", async (req,res) => {
+try{
+const {listingId, startDate, endDate} = req.body;
+if (!listingId || !startDate || !endDate){
+    return res.status(400).json({message: "Missing booking info"})
+}
+const listing = await Listing.findById(listingId);
+    if (!listing) return res.status(404).json({ message: "Listing not found" });
+    const overlap = await Booking.findOne({
+        listingId,
+        $or: [
+            {startDate: {$lte:endDate, $gte:startDate}},
+            {endDate: {$lte:startDate, $gte:endDate}},
+            {startDate: {$lte:startDate}, endDate:{$gte: endDate}},
+        ]
+    });
+    if (overlap) return res.status(400).json({message: "This date range is already booked"})
+  
+}
+ 
+catch (err) {
+    res.status(500).json({message: err.message})
+}
+})
+module.exports = router;
